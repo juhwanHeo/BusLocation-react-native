a2343440937e732e3761d12adf8b2cb20d0e1e47
juhwanHeo <juhwanheo.dev@gmail.com>
2022-07-17 01:04:17 +0900

    22.07.17 - 1 (desktop)
    # DO
    1, MapView 정리
    2, Axios 추가
    
    # TODO
    1, API 연동

--- busLocationProject/Components/View/MapView.js
+++ busLocationProject/Components/View/MapView.js
@@ -1,34 +1,38 @@
-
 import React from 'react';
-import NaverMapView, {Circle, Marker, Path, Polygon, Polyline} from "react-native-nmap";
-import {Text} from "react-native";
+import NaverMapView, {Marker} from "react-native-nmap";
 
-// https://webruden.tistory.com/302
+function MyMap(props) {
+    const locations = props.locations;
 
-function MyMap() {
-    const P0 = {latitude: 37.564362, longitude: 126.977011};
-    const P1 = {latitude: 37.565051, longitude: 126.978567};
-    const P2 = {latitude: 37.565383, longitude: 126.976292};
-
-    return <NaverMapView style={{width: '100%', height: '100%'}}
-                         showsMyLocationButton={true}
-                         center={{...P0, zoom: 16}}
-                         onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
-                         onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
-                         onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}>
-        <Marker coordinate={P0} onClick={() => console.warn('onClick! p0')}/>
-        <Marker coordinate={P1} pinColor="blue" onClick={() => console.warn('onClick! p1')}/>
-        <Marker coordinate={P2} pinColor="red" onClick={() => console.warn('onClick! p2')}/>
-        <Path coordinates={[P0, P1]} onClick={() => console.warn('onClick! path')} width={10}/>
-        <Polyline coordinates={[P1, P2]} onClick={() => console.warn('onClick! polyline')}/>
-        <Circle coordinate={P0} color={"rgba(255,0,0,0.3)"} radius={200} onClick={() => console.warn('onClick! circle')}/>
-        <Polygon coordinates={[P0, P1, P2]} color={`rgba(0, 0, 0, 0.5)`} onClick={() => console.warn('onClick! polygon')}/>
-    </NaverMapView>
-}
-const MapView = () => {
     return (
-        <MyMap />
-        // <Text>test</Text>
+        <NaverMapView style={{width: '100%', height: '100%'}}
+                      showsMyLocationButton={true}
+                      center={{...locations[0], zoom: 16}}
+            // onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
+            // onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
+            // onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}
+        >
+            {locations.map((location) => {
+                return (<Marker coordinate={location}/>);
+            })}
+            {/*<Marker coordinate={P0} onClick={() => console.warn('onClick! p0')}/>*/}
+            {/*<Marker coordinate={P1} pinColor="blue" onClick={() => console.warn('onClick! p1')}/>*/}
+            {/*<Marker coordinate={P2} pinColor="red" onClick={() => console.warn('onClick! p2')}/>*/}
+        </NaverMapView>
+    );
+}
+
+
+const MapView = () => {
+
+    function getLocation() {
+        return [
+            {latitude: 37.565383, longitude: 126.976292}
+        ];
+    }
+
+    return (
+        <MyMap locations={getLocation()}/>
     )
 }
 export default MapView;
